707e55de91bfed66dd728dd93727bc4aaedfe977
malamjayesh <malamj001@gmail.com>
2025-09-30 17:01:43 +0530

    fixing build time error

--- lib/api.ts
+++ lib/api.ts
@@ -160,8 +160,7 @@ export const api = {
     additional_message?: string;
     number_of_samples?: number;
     currency?: string;
-    pin_code?: string;
-    country?: string;
+
   }) {
     try {
       const response = await axios.post(`${API_URL}/api/orders/create`, orderData, {
